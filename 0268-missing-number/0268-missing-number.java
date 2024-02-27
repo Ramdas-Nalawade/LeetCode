ff2acebf22d2b491425c2d895708edf366251fbc
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-02-27 09:31:27 +0530

    Time: 6 ms (14.75%), Space: 45.3 MB (43.28%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) 
+    {
+        Arrays.sort(nums);
+        int num = 0;
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != num)
+            {
+                num = i;
+                break;
+            }
+            num++;
+        }
+        return num;
+    }
+}
