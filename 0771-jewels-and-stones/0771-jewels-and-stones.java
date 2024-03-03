1c9fdb651e78e26cfafa59fb6bf59f17ef17c9f0
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-03-03 19:45:15 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (6.66%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) 
+    {
+        int count = 0;
+        
+        for(char ch: stones.toCharArray())
+        {
+            for(char ch1: jewels.toCharArray())
+            {
+                if(ch == ch1)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
