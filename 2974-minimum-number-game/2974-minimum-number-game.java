ce9c6d01b43891779d1383510937148ecf92b093
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-03-06 14:55:18 +0530

    Time: 2 ms (97.38%), Space: 44.9 MB (27.32%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,13 +1,7 @@
 class Solution {
     public int[] numberGame(int[] nums) 
     {
-        // int[] arr = new int[nums.length];
         Arrays.sort(nums);
-        
-        // for(int i = 0; i < arr.length; i++)
-        // {
-        //     arr[i] = nums[i];
-        // }
         
         for(int i = 0; i < nums.length - 1; i = i + 2)
         {
