f287d110be0866ce67c7b011599851c1c9a5d7eb
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-03-06 06:41:57 +0530

    Time: 1 ms (86.01%), Space: 42.2 MB (59.17%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,17 +1,35 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) 
     {
+//         List<Boolean> list = new ArrayList<>();
+//         int max = 0;
+//         for(int i: candies)
+//         {
+//             if(max < i)
+//                 max = i;
+//         }
+        
+//         for(int i: candies)
+//         {
+//             if((i + extraCandies) >= max)
+//                 list.add(true);
+//             else
+//                 list.add(false);
+//         }
+//         return list;
+        
         List<Boolean> list = new ArrayList<>();
-        int max = 0;
-        for(int i: candies)
-        {
-            if(max < i)
-                max = i;
-        }
+        Integer[] arr = new Integer[candies.length];
+        int count = 0;
         
         for(int i: candies)
+            arr[count++] = i;
+        
+        int max = Collections.max(Arrays.asList(arr));
+        
+        for(int i: arr)
         {
-            if((i + extraCandies) >= max)
+            if((i+extraCandies) >= max)
                 list.add(true);
             else
                 list.add(false);
