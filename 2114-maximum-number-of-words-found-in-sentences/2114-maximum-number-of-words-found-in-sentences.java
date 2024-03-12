02b21e1151151e390e447964f446005807ec82cc
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-03-12 09:55:54 -0700

    Time: 2 ms (97.05%), Space: 44.4 MB (19.46%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,15 +1,14 @@
 class Solution {
     public int mostWordsFound(String[] sentences) 
-    {       
+    {
         int max = 0;
-        
+
         for(String s: sentences)
         {
             int count = s.split(" ").length;
             if(max < count)
-            max = count;
-        }
-        
+                max = count;
+        }        
         return max;
     }
 }
