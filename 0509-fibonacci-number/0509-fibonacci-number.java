961749519f549ec541d17cc10efd9108be7d5b62
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-03-14 18:19:25 -0700

    Time: 0 ms (100%), Space: 40.3 MB (39.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -6,8 +6,9 @@ class Solution {
         if(n == 1)
             return 1;
         
+        int[] arr = new int[n+1];
         arr[1] = 1;
-        
+
         for(int i = 2; i<= n; i++)
         {
             arr[i] = arr[i-1] + arr[i-2];
