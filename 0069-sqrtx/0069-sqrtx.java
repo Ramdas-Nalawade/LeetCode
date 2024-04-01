c448d63b2f67df25604af2d59a7aa6bacd98efca
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2024-04-01 11:23:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,14 @@
 class Solution {
     public int mySqrt(int x) 
     {
-        return (int) Math.sqrt(x);
+        // return (int) Math.sqrt(x);
+
+        double d = x;
+
+        while(d * d > x)
+        {
+            d = (d + x/d) / 2;
+        }
+        return (int) d;
     }
 }
