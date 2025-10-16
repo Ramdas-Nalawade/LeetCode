90c9b7e716bb1c8014300a10ff837f024983f07f
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2025-10-16 14:39:18 +0530

    Time: 1 ms (85.31%), Space: 41.4 MB (13.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,14 +1,9 @@
 class Solution {
     public int mySqrt(int x) 
     {
-        // return (int) Math.sqrt(x);
-
-        double d = x;
-
-        while(d * d > x)
-        {
-            d = (d + x/d) / 2;
-        }
-        return (int) d;
+        long r = x;
+        while(r*r > x)
+        r = (r + x/r)/ 2;
+        return (int) r;      
     }
 }
