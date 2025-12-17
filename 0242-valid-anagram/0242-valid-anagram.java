69e34a55c29616f2a6c2e4968cf8e7a9b153f019
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2025-12-17 14:22:11 +0530

    Time: 4 ms (78.98%), Space: 46 MB (39.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) 
     {
         char[] sChar = s.toCharArray();
-        char[] tChar =t.toCharArray();
+        char[] tChar = t.toCharArray();
 
         Arrays.sort(sChar);
         Arrays.sort(tChar);
