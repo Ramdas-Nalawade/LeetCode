60f0e1b9cee8b62cdb494324787157a9e1f36b1f
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2025-12-19 09:45:18 +0530

    Time: 26 ms (6.18%), Space: 52.3 MB (5.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,50 @@
 class Solution {
-    public boolean isAnagram(String s, String t) 
-    {
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
 
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
+        Map<Integer, Long> sMap = s.chars()
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        c -> c,
+                        Collectors.counting()
+                ));
 
-        return Arrays.equals(sChar, tChar);
+        Map<Integer, Long> tMap = t.chars()
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        c -> c,
+                        Collectors.counting()
+                ));
+
+        return sMap.equals(tMap);
     }
 }
+
+
+//Using Map
+/*
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> counter = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
+                return false;
+            }
+            counter.put(ch, counter.get(ch) - 1);
+        }
+
+        return true;        
+    }
+}
+*/
