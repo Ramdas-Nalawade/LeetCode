ac75e4a2a547fff5176b525091a6531f41217971
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-01-05 14:59:07 +0530

    Time: 1 ms (74.08%), Space: 46.7 MB (41.23%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -12,16 +12,28 @@ class Solution {
         // }
         // return count;
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int count = 0;
 
-        for(int i = 0; i < nums.length; i++)
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+        //     if(map.get(nums[i]) == 1)
+        //         nums[count++] = nums[i];
+        // }
+        // return count;
+
+        int i = 0; 
+
+        for(int j = 0; j < nums.length; j++)
         {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-
-            if(map.get(nums[i]) == 1)
-                nums[count++] = nums[i];
+            if(nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
         }
-        return count;
+        return i+1;
     }
 }
