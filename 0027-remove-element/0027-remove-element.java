0d4796d9511ea4ffeb1e4e241afe5d9f426f5be8
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-01-05 22:17:48 +0530

    Time: 0 ms (100%), Space: 43.5 MB (60.5%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -6,8 +6,8 @@ class Solution {
         for(int i = 0; i < nums.length; i++)
         {
             if(nums[i] != val)
-                nums[count++] = nums[i];
-        }        
+            nums[count++] = nums[i];
+        }
         return count;
     }
 }
