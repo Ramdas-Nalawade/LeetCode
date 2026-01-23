e13026794d62840e4ddf4d2918c958f9a4e6a53d
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-01-23 13:02:19 +0530

    Time: 31 ms (39.42%), Space: 46.9 MB (62.09%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -7,7 +7,7 @@ class Solution {
         {
             char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
+        }        
         for(int i = 0; i < s.length(); i++)
         {
             char ch = s.charAt(i);
