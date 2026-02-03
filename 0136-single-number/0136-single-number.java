d1bdfeeeaf8eb429951e7e343410efa91c57089d
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-03 14:21:39 +0530

    Time: 12 ms (22.46%), Space: 47 MB (60.33%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -4,11 +4,12 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i: nums)
+        {
             map.put(i, map.getOrDefault(i, 0) + 1);
-
+        }        
         for(int i: nums)
         {
-            if(map.containsKey(i) && map.get(i) == 1)
+            if(map.get(i) == 1)
                 return i;
         }
         return -1;
