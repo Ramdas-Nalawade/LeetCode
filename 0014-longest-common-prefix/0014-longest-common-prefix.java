6688e651c41d882a2768303d7682ca24bd25417b
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-03 12:26:54 +0530

    Time: 1 ms (65.53%), Space: 43.1 MB (79.6%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,7 +1,8 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) 
     {
-        if(strs == null || strs.length == 0) return "";
+        if(strs == null || strs.length == 0) 
+            return "";
 
         String first = strs[0];
 
@@ -9,7 +10,8 @@ class Solution {
         {
             for(int j = 1; j < strs.length; j++)
             {
-                if(i == strs[j].length() || first.charAt(i) != strs[j].charAt(i))
+                if(i == strs[j].length() || 
+                first.charAt(i) != strs[j].charAt(i))
                     return first.substring(0, i);
             }
         }
