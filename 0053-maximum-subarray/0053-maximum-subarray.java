e0c73fa5dab7fbfc1326b49dfd65ba85875adf6e
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-05 12:40:04 +0530

    Time: 1 ms (99.58%), Space: 77.2 MB (62.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 1; i < nums.length; i++)
         {
             currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+            maxSum = Math.max(currentSum, maxSum);
         }
         return maxSum;
     }
