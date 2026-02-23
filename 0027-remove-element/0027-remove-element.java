e1d8da53e53de80bda9f8de7f0b9374e4891af3d
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-23 14:20:51 +0530

    Time: 0 ms (100%), Space: 43.5 MB (80.03%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,23 +1,18 @@
 class Solution {
     public int removeElement(int[] nums, int val) 
     {
-        // int count = 0;
-        // Set<Integer> set = new HashSet<>();
-
-        // for(int i = 0; i < nums.length; i++)
-        // {
-        //     if(nums[i] != val)
-        //         nums[count++] = nums[i];
-        //     set.add(nums[i]);
-        // }        
-        // return count;
-
         int count = 0;
+
         for(int i = 0; i < nums.length; i++)
         {
             if(nums[i] != val)
                 nums[count++] = nums[i];
-        }
+                
+        }      
+        for(int i = count; i < nums.length; i++)
+        {
+            nums[i] = 0;
+        }  
         return count;
     }
 }
