c4a8cfc47295f2252b4dd802c22d520ef95e58bd
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-23 14:07:11 +0530

    Time: 6 ms (3.87%), Space: 46.8 MB (63.3%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,39 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) 
     {
-        // Set<Integer> set = new HashSet<>();
-        // int count = 0;
-
-        // for(int i: nums)
-        // {
-        //     if(!set.contains(i))
-        //         nums[count++] = i;
-        //     set.add(i);
-        // } 
-        // return count;
-
-        // Map<Integer, Integer> map = new HashMap<>();
-        // int count = 0;
-
-        // for(int i: nums)
-        // {
-        //     map.put(i, map.getOrDefault(i, 0) + 1);
-        //     if(map.get(i) == 1)
-        //         nums[count++] = i;
-        // }
-
-        // return count;
-
-        int j = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
 
         for(int i = 0; i < nums.length; i++)
         {
-            if(nums[i] != nums[j])
-            {
-                j++;
-                nums[j] = nums[i];
-            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(map.get(nums[i]) == 1)
+                nums[count++] = nums[i];
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) - 1);
         }
-        return j+1;
+        return count;
     }
 }
