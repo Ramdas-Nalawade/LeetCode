51df85fe12c7df5d2f3e85c48b7a4760e7733174
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-24 23:36:15 +0530

    Time: 2 ms (97.19%), Space: 45.2 MB (25.25%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,26 +1,45 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) 
     {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
+        // Map<Integer, Integer> map = new HashMap<>();
+        // List<Integer> list = new ArrayList<>();
+
+        // for(int i: nums1)
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
+
+        // for(int i: nums2)
+        // {
+        //     if(map.getOrDefault(i, 0) > 0)
+        //     {
+        //         if(!list.contains(i))
+        //         list.add(i);
+        //         map.put(i, map.getOrDefault(i, 0) - 1);
+        //     }
+        // }
+        // int[] arr = new int[list.size()];
+
+        // for(int i = 0; i < list.size(); i++)
+        //     arr[i] = list.get(i);
+
+        // return arr;
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
 
         for(int i: nums1)
-            map.put(i, map.getOrDefault(i, 0) + 1);
+            set1.add(i);
 
         for(int i: nums2)
+            if(set1.contains(i))
+                set2.add(i);
+
+        int[] arr = new int[set2.size()];
+        int i = 0;
+
+        for(int j: set2)
         {
-            if(map.getOrDefault(i, 0) > 0)
-            {
-                if(!list.contains(i))
-                list.add(i);
-                map.put(i, map.getOrDefault(i, 0) - 1);
-            }
+            arr[i++] = j;
         }
-        int[] arr = new int[list.size()];
-
-        for(int i = 0; i < list.size(); i++)
-            arr[i] = list.get(i);
-
         return arr;
     }
 }
