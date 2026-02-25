dbe6bd513c19f89448713c0324da465138cb97c4
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-02-25 14:33:53 +0530

    Time: 2 ms (21.89%), Space: 44.1 MB (25.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -11,18 +11,18 @@ class Solution {
         {
             nums1[count++] = nums2[i];
         }
-        Arrays.sort(nums1);
-        // for(int i = 0; i < nums1.length - 1; i++)
-        // {
-        //     for(int j = 0; j < nums1.length - 1 - i; j++)
-        //     {
-        //         if(nums1[j] > nums1[j+1])
-        //         {
-        //             int temp = nums1[j];
-        //             nums1[j] = nums1[j+1];
-        //             nums1[j+1] = temp;
-        //         }
-        //     }
-        // }
+        // Arrays.sort(nums1);
+        for(int i = 0; i < nums1.length - 1; i++)
+        {
+            for(int j = 0; j < nums1.length - 1 - i; j++)
+            {
+                if(nums1[j] > nums1[j+1])
+                {
+                    int temp = nums1[j];
+                    nums1[j] = nums1[j+1];
+                    nums1[j+1] = temp;
+                }
+            }
+        }
     }
 }
