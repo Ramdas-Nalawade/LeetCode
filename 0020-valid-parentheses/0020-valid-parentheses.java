5502bbc2a0ba638d8e8ba33807f424fe8fa858ab
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-04 20:34:27 +0530

    Time: 2 ms (97.7%), Space: 43.3 MB (73.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,19 +3,17 @@ class Solution {
     {
         Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < s.length(); i++)
+        for(char ch: s.toCharArray())
         {
-            char ch = s.charAt(i);
-
             if(ch == '(')
                 stack.push(')');
             else if(ch == '{')
                 stack.push('}');
             else if(ch == '[')
                 stack.push(']');
-            else if(stack.isEmpty() || stack.pop() != ch)
+            else if(stack.empty() || stack.pop() != ch)
                 return false;
-        }        
-        return stack.isEmpty();
+        }  
+        return stack.empty();
     }
 }
