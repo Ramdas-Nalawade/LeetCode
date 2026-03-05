5242534f49b231bfb5fe7058cedf1741c310c8f1
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-05 16:59:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -7,19 +7,10 @@ class Solution {
             set.add(i);
 
         Integer[] arr = set.toArray(new Integer[0]);
-        for(int i = 0; i < arr.length - 1; i++)
-        {
-            for(int j = 0; j < arr.length - 1 - i; j++)
-            {
-                if(arr[j] > arr[j+1])
-                {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
+        Arrays.sort(arr);
 
-        return arr.length > 2 ? arr[arr.length - 3] : arr[arr.length - 1];
+        if(arr.length < 3)
+            return arr[arr.length - 1];
+        return arr[arr.length - 3];        
     }
 }
