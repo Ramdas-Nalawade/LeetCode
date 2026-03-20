3798e2550ac91bfc83b71f7b971e8fa8a3b96fc2
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-20 22:35:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) 
     {
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++)
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     int ch = nums[i];
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        //     if(map.get(ch) >= 2)
+        //         return true;
+        // }
+        // return false;
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 1; i++)
         {
-            int ch = nums[i];
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-            if(map.get(ch) >= 2)
+            if(nums[i] == nums[i+1])
                 return true;
         }
         return false;
