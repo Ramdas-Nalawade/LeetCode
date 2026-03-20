a8afe528f9292c6b8a2190900e57355860134b68
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-20 22:52:25 +0530

    Time: 3 ms (6.21%), Space: 43.8 MB (5.27%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,18 +1,16 @@
 class Solution {
     public String toLowerCase(String s) 
     {
-        String result = "";
+        String ans = "";
 
         for(int i = 0; i < s.length(); i++)
         {
             char ch = s.charAt(i);
             if(ch >= 'A' && ch <= 'Z')
-            {
-                result += (char) (ch+32);
-            }
+                ans += (char) (ch+32);
             else
-                result += ch;
-        }        
-        return result;
+                ans += ch;
+        }
+        return ans;
     }
 }
