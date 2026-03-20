f35ec78c2064b4e593cb311eb2ba4016c6d49236
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-20 15:29:21 +0530

    Time: 0 ms (100%), Space: 43 MB (65.08%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,17 +2,17 @@ class Solution {
     public int strStr(String haystack, String needle) 
     {
         if(needle.length() > haystack.length() || 
-        !haystack.contains(needle))        
+            !haystack.contains(needle))
             return -1;
-
+        
         for(int i = 0; i <= haystack.length() - needle.length(); i++)
         {
             int j = 0;
 
-            while(j < needle.length() && 
-                haystack.charAt(i+j) == needle.charAt(j))
+            while(j < needle.length() && haystack.charAt(i+j) 
+            == needle.charAt(j))
                 j++;
-
+            
             if(j == needle.length())
                 return i;
         }
