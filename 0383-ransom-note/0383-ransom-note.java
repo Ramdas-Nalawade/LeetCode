2e87243228f1be889968e56cde41c6090469e366
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-21 17:18:01 +0530

    Time: 11 ms (43.02%), Space: 46.9 MB (40.2%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -7,15 +7,14 @@ class Solution {
         {
             char ch = magazine.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }        
+        }
         for(int i = 0; i < ransomNote.length(); i++)
         {
             char ch = ransomNote.charAt(i);
-            if(!map.containsKey(ch) ||  map.get(ch) == 0)
+            if(!map.containsKey(ch) || map.get(ch) == 0)
                 return false;
             map.put(ch, map.getOrDefault(ch, 0) - 1);
         }
         return true;
-
     }
 }
