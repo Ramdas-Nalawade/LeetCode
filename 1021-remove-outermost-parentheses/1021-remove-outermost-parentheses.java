b6a9dd743e675bfb4b360b1222c2d7999f39de3f
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-21 18:02:19 +0530

    Time: 11 ms (10.91%), Space: 46.6 MB (15.9%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,26 +1,25 @@
 class Solution {
     public String removeOuterParentheses(String s) 
     {
-        //when depth becomes 1 → 0 or 0 → 1,
-        //we know we are crossing the outer wall → skip those parentheses.
-        int depth = 0;
-        StringBuilder result = new StringBuilder();
+        String result = "";
+        int count = 0;
 
-        for(char ch: s.toCharArray())
+        for(int i = 0; i < s.length(); i++)
         {
+            char ch = s.charAt(i);
             if(ch == '(')
             {
-                if(depth > 0)
-                    result.append(ch);
-                depth++;
+                if(count > 0)
+                    result += ch;
+                count++;
             }
             else
             {
-                depth--;
-                if(depth > 0)
-                    result.append(ch);
+                count--;
+                if(count > 0)
+                    result += ch;
             }
-        }    
-        return result.toString();    
+        }   
+        return result;
     }
 }
