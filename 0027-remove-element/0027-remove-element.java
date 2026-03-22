45da61ca49d72d849c364006f353dca250ca17c1
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-03-22 18:57:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int removeElement(int[] nums, int val) 
-    {
+    public int removeElement(int[] nums, int val) {
         int p=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i] != val){
