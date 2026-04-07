17657ce7dba1c4cfa833d6d4b0680977e3e8985a
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-04-07 18:28:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,29 @@
 class Solution {
     public boolean canJump(int[] nums) 
     {
-        int goal = nums.length - 1;
+        int goal = 0;
 
-        for(int i = nums.length - 2; i >= 0; i--)
+        for(int i = 0; i < nums.length; i++)
         {
-            if((i+nums[i]) >= goal)
-                goal = i;
+            if(i > goal)
+                return false;
+
+            goal = Math.max(goal ,i+nums[i]);
         }
-        return goal == 0;
+        return true;
     }
 }
+
+// class Solution {
+//     public boolean canJump(int[] nums) 
+//     {
+//         int goal = nums.length - 1;
+
+//         for(int i = nums.length - 2; i >= 0; i--)
+//         {
+//             if((i+nums[i]) >= goal)
+//                 goal = i;
+//         }
+//         return goal == 0;
+//     }
+// }
