1e434857f9af52fe2f947e85c95c3d195796ab2a
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-04-16 16:37:22 +0530

    Time: 31 ms (38.69%), Space: 46.7 MB (93.47%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,15 +3,13 @@ class Solution {
     {
         Map<Character, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++)
-        {
-            char ch = s.charAt(i);
+        for(char ch: s.toCharArray())
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
+
         for(int i = 0; i < s.length(); i++)
         {
             char ch = s.charAt(i);
-            if(map.containsKey(ch) && map.get(ch) == 1)
+            if(map.get(ch) == 1)
                 return i;
         }
         return -1;
