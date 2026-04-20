446c66bbaed86933fc860cc8ca2dafefcc638cda
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-04-20 22:39:14 +0530

    Time: 0 ms (100%), Space: 43.4 MB (24.34%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,20 +1,31 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) 
     {
+        // int count = 0;
+
+        // Map<Character, Integer> map = new HashMap<>();
+
+        // for(int i = 0; i < jewels.length(); i++)
+        // {
+        //     char ch = jewels.charAt(i);
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // for(int i = 0; i < stones.length(); i++)
+        // {
+        //     char ch = stones.charAt(i);
+        //     if(map.containsKey(ch) && map.get(ch) >= 1)
+        //         count++;
+        // }
+        // return count;
+        
         int count = 0;
-
-        Map<Character, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < jewels.length(); i++)
+        for(char ch: stones.toCharArray())
         {
-            char ch = jewels.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        for(int i = 0; i < stones.length(); i++)
-        {
-            char ch = stones.charAt(i);
-            if(map.containsKey(ch) && map.get(ch) >= 1)
-                count++;
+            for(char ch2: jewels.toCharArray())
+            {
+                if(ch == ch2)
+                    count++;
+            }
         }
         return count;
     }
