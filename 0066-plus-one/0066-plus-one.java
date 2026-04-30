77588c6bc029df499569095d58cc53f9a161855e
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-04-30 22:33:52 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.78%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) 
     {
-        for(int i = digits.length - 1; i >= 0; i--)
+        for(int i = digits.length-1; i >= 0; i--)
         {
             if(digits[i] < 9)
             {
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
+            else
+            {
+                digits[i] = 0;
+            }
         }
-        int[] arr = new int[digits.length+1];
-        arr[0] = 1;
-        return arr;
+        digits = new int[digits.length+1];
+        digits[0] = 1;
+        return digits;
     }
 }
