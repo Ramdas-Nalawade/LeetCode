01d8d5a13a3c2814c9c1e93214dd74ae7c4c9213
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-05-04 13:53:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) 
     {
-        // Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
 
-        // for(int i: nums)
-        // {
-        //     if(set.contains(i))
-        //         return true;
-        //     set.add(i);
-        // }
-        // return false;
+        for(int i: nums)
+        {
+            if(set.contains(i))
+                return true;
+            set.add(i);
+        }
+        return false;
 
         // Arrays.sort(nums);
 
@@ -20,18 +20,17 @@ class Solution {
         // }
         // return false;
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i: nums)
-        {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(map.get(nums[i]) >= 2)
-                return true;
-        }
-        return false;
-
+        // for(int i: nums)
+        // {
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
+        // }
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     if(map.get(nums[i]) >= 2)
+        //         return true;
+        // }
+        // return false;
     }
 }
