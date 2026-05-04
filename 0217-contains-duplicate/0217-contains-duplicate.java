1229b2cdeed3982e99e57cafccd62d0bf0c712f3
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-05-04 13:52:46 +0530

    Time: 38 ms (5.07%), Space: 96.8 MB (15.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -11,13 +11,27 @@ class Solution {
         // }
         // return false;
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length - 1; i++)
+        // for(int i = 0; i < nums.length - 1; i++)
+        // {
+        //     if(nums[i] == nums[i+1])
+        //         return true;
+        // }
+        // return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i: nums)
         {
-            if(nums[i] == nums[i+1])
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(map.get(nums[i]) >= 2)
                 return true;
         }
         return false;
+
     }
 }
