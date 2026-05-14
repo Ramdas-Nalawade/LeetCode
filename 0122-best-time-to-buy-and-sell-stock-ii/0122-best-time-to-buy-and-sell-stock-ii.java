8464142d52284058cbfc6c2e465b0e7eea35b363
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-05-14 14:10:22 +0530

    Time: 0 ms (100%), Space: 46.3 MB (81.99%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -13,3 +13,13 @@ class Solution {
         return maxProfit;
     }
 }
+
+        // int maxProfit = 0;
+
+        // for(int i = 1; i < prices.length; i++)
+        // {
+        //     int currentProfit = prices[i] - prices[i-1];
+        //     if(currentProfit > 0)
+        //         maxProfit += currentProfit;
+        // }
+        // return maxProfit;
