fb4174e9f9d3baa7660e5a991e20d2db5156e106
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-05-15 15:11:40 +0530

    Time: 5 ms (67.84%), Space: 43.7 MB (28.16%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,19 @@
 class Solution {
     public int hIndex(int[] citations) 
     {
-        for(int i = 0; i < citations.length; i++)
-        {
-            for(int j = 0; j < citations.length - 1 - i; j++)
-            {
-                if(citations[j] > citations[j+1])
-                {
-                    int temp = citations[j];
-                    citations[j] = citations[j+1];
-                    citations[j+1] = temp;
-                }
-            }
-        }
+        // for(int i = 0; i < citations.length; i++)
+        // {
+        //     for(int j = 0; j < citations.length - 1 - i; j++)
+        //     {
+        //         if(citations[j] > citations[j+1])
+        //         {
+        //             int temp = citations[j];
+        //             citations[j] = citations[j+1];
+        //             citations[j+1] = temp;
+        //         }
+        //     }
+        // }
+        Arrays.sort(citations);
         int n = citations.length;
 
         for(int i = 0; i < citations.length; i++)
