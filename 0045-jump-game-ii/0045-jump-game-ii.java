b8cb17ab47fcecf4ca2a95513b8ac7ec48c7c932
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-05-15 14:46:35 +0530

    Time: 1 ms (99.6%), Space: 46.7 MB (98.68%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public int jump(int[] nums) 
     {
-        int jumps = 0, currentEnd = 0, farthest = 0;
+        int jumps = 0, farthest = 0, current = 0;
 
         for(int i = 0; i < nums.length - 1; i++)
         {
             farthest = Math.max(farthest, i+nums[i]);
 
-            if(i == currentEnd)
+            if(i == current)
             {
                 jumps++;
-                currentEnd = farthest;
+                current = farthest;
+
+                if(current >= nums.length - 1)
+                    break;
             }
         }
         return jumps;
