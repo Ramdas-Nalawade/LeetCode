1a44ce9ffd92b925f40932141fe66f7187bb7a6b
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-05-31 12:46:32 +0530

    Time: 1 ms (98.44%), Space: 47.6 MB (21.72%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,22 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) 
     {
-        int[] leftArr = new int[nums.length];
-        int[] rightArr = new int[nums.length];
-        leftArr[0] = nums[0];
-        rightArr[nums.length-1] = nums[nums.length-1];
+        int n = nums.length;
+        int[] leftSum = new int[n], rightSum = new int[n];
+        leftSum[0] = nums[0];
+        rightSum[n-1] = nums[n-1];
 
-        for(int i = 1; i < nums.length; i++)
+        for(int i = 1; i < n; i++)
+            leftSum[i] = leftSum[i-1] + nums[i];
+
+        for(int i = n - 2; i >= 0; i--)
+            rightSum[i] = rightSum[i+1] + nums[i];
+
+        for(int i = 0; i < n; i++)
         {
-            leftArr[i] = leftArr[i-1] + nums[i];
-        }        
-        for(int i = nums.length-2; i >= 0; i--)
-        {
-            rightArr[i] = rightArr[i+1] + nums[i];
-        } 
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(leftArr[i]-nums[i] == rightArr[i]-nums[i])
+            if((leftSum[i]-nums[i]) == (rightSum[i]-nums[i]))
                 return i;
         }
         return -1;
