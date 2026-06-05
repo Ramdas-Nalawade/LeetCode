14d1100612fa0df0392227794dc2bc0f7d9d1afa
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-06-05 20:37:59 +0530

    Time: 16 ms (13.26%), Space: 47.5 MB (10.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) 
     {
-        String arr = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        StringBuilder sb = new StringBuilder(arr);
-        
-        return sb.reverse().toString().equals(arr);
+        String uS = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        StringBuilder sb = new StringBuilder(uS);
+        return sb.reverse().toString().equals(uS);
     }
 }
