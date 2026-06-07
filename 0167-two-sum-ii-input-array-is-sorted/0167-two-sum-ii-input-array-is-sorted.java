d9681bc4608f4663b8079797516620a9ff8e23d4
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-06-07 16:19:37 +0530

    Time: 6 ms (13.17%), Space: 49.3 MB (5.67%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -8,10 +8,11 @@ class Solution {
             int complement = target - numbers[i];
 
             if(map.containsKey(complement))
+            {
                 return new int[] {map.get(complement)+1, i+1};
-
+            }
             map.put(numbers[i], i);
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
