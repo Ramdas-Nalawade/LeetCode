e6a1bc696dfd376874475e5428a8a15d3be026fc
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-06-15 22:41:03 +0530

    Time: 18 ms (13.97%), Space: 46.7 MB (15.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
-        // if(s.length() != t.length())
-        //     return false;
+        if(s.length() != t.length())
+            return false;
 
-        // Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
 
-        // for(char ch: s.toCharArray())
-        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for(int i = 0; i < t.length(); i++)
+        {
+            char ch = t.charAt(i);
+            if(!map.containsKey(ch) || map.get(ch) == 0)
+                return false;
 
-        // for(char ch: t.toCharArray())
-        // {
-        //     if(!map.containsKey(ch) || map.get(ch) == 0)
-        //         return false;
-        //     map.put(ch, map.getOrDefault(ch, 0) - 1);
-        // }
-        // return true;
-
-        char[] arrS = s.toCharArray();
-        char[] arrT = t.toCharArray();
-        Arrays.sort(arrS);
-        Arrays.sort(arrT);
-
-        return Arrays.equals(arrS, arrT);
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
+        }
+        return true;
     }
 }
