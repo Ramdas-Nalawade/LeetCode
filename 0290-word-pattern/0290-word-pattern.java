17703c613438b0e9d4230421afeda80d73b31135
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-06-15 22:25:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -7,14 +7,14 @@ class Solution {
         for(int i = 0; i < pattern.length(); i++)
         {
             char ch = pattern.charAt(i);
-            String temp = (arr[i]);
+            StringBuilder temp = new StringBuilder(arr[i]);
             
             if(map.containsValue(temp) && !map.containsKey(ch))
                 return false;
-            else if(map.containsKey(ch) && !map.get(ch).equals(temp))
+            else if(map.containsKey(ch) && !map.get(ch).equals(temp.toString()))
                 return false;
             else
-                map.put(ch, temp);
+                map.put(ch, temp.toString());
         }
         return true;
     }
