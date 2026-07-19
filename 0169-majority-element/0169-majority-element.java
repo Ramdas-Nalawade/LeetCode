9e6d5b65d0ed9503bb190cf5e1e2e84861ce4838
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-07-19 16:56:16 +0530

    Time: 17 ms (16.26%), Space: 52.8 MB (76.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,11 +3,11 @@ class Solution {
     {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++)
+        for(int i: nums)
         {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(map.get(nums[i]) > nums.length/2)
-                return nums[i];
+            map.put(i, map.getOrDefault(i, 0) + 1);
+            if(map.get(i) > nums.length/2)
+                return i;
         }
         return -1;
     }
