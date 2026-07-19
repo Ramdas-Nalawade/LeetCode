a2b4111019a85c8e6f2779259ca5b04926149ad8
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-07-19 16:43:51 +0530

    Time: 6 ms (4.59%), Space: 47.6 MB (98.9%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,45 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) 
     {
+        Map<Integer, Integer> map = new LinkedHashMap<>();
         int index = 0;
-        Map<Integer, Integer> map = new HashMap<>();
 
         for(int i: nums)
+            map.put(i, map.getOrDefault(i, 0) + 1);
+
+        for(int i = 0; i < nums.length; i++)
         {
-            if(map.getOrDefault(i, 0) < 2)
-            {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-                nums[index++] = i;
-            }
+            int ch = nums[i];
+            if(map.get(ch) <= 2)
+                nums[index++] = nums[i];
+            else
+                map.put(ch, map.getOrDefault(ch, 0) - 1);
         }
         return index;
-
-// Map<Integer, Integer> map = new HashMap<>();
-        // int count = 0;
-
-        // for(int i: nums)
-        //     map.put(i, map.getOrDefault(i, 0) + 1);
-
-        // for(int i = 0; i < nums.length; i++)
-        // {
-        //     if(map.get(nums[i]) <= 2)
-        //     {
-        //         nums[count++] = nums[i];
-        //     }
-        //     map.put(nums[i], map.getOrDefault(nums[i], 0) - 1);
-        // }
-        // return count;
-
-        // int k = 2;
-
-        // for(int i = 2; i < nums.length; i++)
-        // {
-        //     if(nums[i] != nums[k-2])
-        //     {
-        //         nums[k] = nums[i];
-        //         k++;
-        //     }
-        // }
-        // return k;
     }
 }
