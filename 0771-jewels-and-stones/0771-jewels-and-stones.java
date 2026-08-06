c83f1d4caa773a3e9e56e2f20da68bdffb5afbb5
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-08-06 12:22:18 +0530

    Time: 2 ms (12.18%), Space: 43.4 MB (21.01%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -3,13 +3,25 @@ class Solution {
     {
         int count = 0;
 
+        // for(char ch: jewels.toCharArray())
+        // {
+        //     for(char c: stones.toCharArray())
+        //     {
+        //         if(ch == c)
+        //             count++;
+        //     }
+        // }
+        // return count;
+
+        Map<Character, Integer> map = new HashMap<>();
+
         for(char ch: jewels.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+        for(char c: stones.toCharArray())
         {
-            for(char c: stones.toCharArray())
-            {
-                if(ch == c)
-                    count++;
-            }
+            if(map.containsKey(c) && map.get(c) >= 1)
+                count++;
         }
         return count;
     }
