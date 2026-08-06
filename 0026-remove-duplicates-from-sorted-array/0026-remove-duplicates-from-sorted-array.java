4ee0a394033154d2c7a917bd49c71bef7a5f874c
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-08-06 16:17:06 +0530

    Time: 3 ms (5.88%), Space: 46.8 MB (24.97%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,16 +2,24 @@ class Solution {
     public int removeDuplicates(int[] nums) 
     {
         int count = 0;
-        Set<Integer> set = new LinkedHashSet<>();
+        // Set<Integer> set = new LinkedHashSet<>();
+
+        // for(int i: nums)
+        //     set.add(i);
+
+        // for(int i: set)
+        // {
+        //     nums[count++] = i;
+        // }
+        // return count;
+
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int i: nums)
-            set.add(i);
-
-        // int[] arr = set.stream().mapToInt(Integer::intValue).toArray();
-
-        for(int i: set)
         {
-            nums[count++] = i;
+            if(!map.containsKey(i))
+                nums[count++] = i;
+            map.put(i, 0);
         }
         return count;
     }
