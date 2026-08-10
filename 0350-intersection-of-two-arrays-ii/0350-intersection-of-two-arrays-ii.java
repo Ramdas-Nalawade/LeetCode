0969fe6ccb6b825c62c30bde7700069453b17c0d
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-08-10 11:35:33 +0530

    Time: 6 ms (34.77%), Space: 45.3 MB (9.9%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -9,19 +9,12 @@ class Solution {
 
         for(int i: nums2)
         {
-            if(map.getOrDefault(i, 0) > 0)
+            if(map.containsKey(i) && map.get(i) >= 1)
             {
                 list.add(i);
                 map.put(i, map.getOrDefault(i, 0) - 1);
             }
         }
-        int[] ans = new int[list.size()];
-
-        for(int i = 0; i < list.size(); i++)
-        {
-            ans[i] = list.get(i);
-        }
-        return ans;
-        // return list.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
