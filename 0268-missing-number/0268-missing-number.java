9b08e3271aca93ef740a0ce55a8567193e25548a
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-08-18 19:03:12 +0530

    Time: 0 ms (100%), Space: 47.2 MB (67.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,37 @@
 class Solution {
     public int missingNumber(int[] nums) 
     { 
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-        int curr = 1;
+        // Map<Integer, Integer> map = new LinkedHashMap<>();
+        // int curr = 1;
+
+        // for(int i: nums)
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
+
+        // for(int i = 1; i <= nums.length; i++)
+        // {
+        //     if(!map.containsKey(i))
+        //         return i;
+        // }
+        // return 0;
+
+        // Arrays.sort(nums);
+        // int curr = 1;
+
+        // for(int i = 1; i < nums.length; i++)
+        // {
+        //     if(nums[curr++] != i)
+        //         return i;
+        // }
+        // return 0;
+
+        int n = nums.length;
+        int sum = (n * (n+1)) /2;
+        int actualSum = 0;
 
         for(int i: nums)
-            map.put(i, map.getOrDefault(i, 0) + 1);
+            actualSum += i;
 
-        for(int i = 1; i <= nums.length; i++)
-        {
-            if(!map.containsKey(i))
-                return i;
-        }
-        return 0;
+        return sum - actualSum;
+
     }
 }
