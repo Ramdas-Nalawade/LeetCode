94c5d7bff8842f8d927f214a80d8b9a04456ed0b
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2026-09-22 15:42:56 +0530

    Time: 0 ms (100%), Space: 42.2 MB (87.69%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -7,8 +7,7 @@ class Solution {
         {
             columnNumber--;
             int mod = columnNumber % 26;
-            char c = (char) (mod+'A');
-            sb.append(c);
+            sb.append((char) (mod+'A'));
             columnNumber /= 26;
         }
         return sb.reverse().toString();
